c674877a80f5989b4c04359c3a9b81725233d03f
pdamianik <39028343+pdamianik@users.noreply.github.com>
2023-10-26 21:52:04 +0200

    Add events to Statistic

--- src/model/statistic.rs
+++ src/model/statistic.rs
@@ -2,15 +2,23 @@ use std::fmt::Debug;
 use std::sync::Arc;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use futures::StreamExt;
-use pharos::Events;
+use pharos::{Events, Observable, Observe, ObserveConfig, PharErr, SharedPharos};
 use tokio::spawn;
 use tokio::task::JoinHandle;
 use crate::model::TrainerEvent;
+
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub enum StatisticEvent {
+    Correct(usize),
+    Incorrect(usize),
+}
 
 #[derive(Debug, Default)]
 pub struct Statistic {
     correct: AtomicUsize,
     incorrect: AtomicUsize,
+    events: SharedPharos<StatisticEvent>,
 }
 
 impl Statistic {
@@ -24,6 +32,18 @@ impl Statistic {
 
     pub fn incorrect(&self) -> usize {
         self.incorrect.load(Ordering::Relaxed)
+    }
+
+    pub async fn observe(&self, options: ObserveConfig<StatisticEvent>) -> Result<Events<StatisticEvent>, PharErr> {
+        self.events.observe_shared(options).await
+    }
+}
+
+impl Observable<StatisticEvent> for Statistic {
+    type Error = PharErr;
+
+    fn observe(&mut self, options: ObserveConfig<StatisticEvent>) -> Observe<'_, StatisticEvent, Self::Error> {
+        self.events.observe(options)
     }
 }
 
@@ -50,9 +70,11 @@ impl StatisticReceiver {
                 match event {
                     Some(TrainerEvent::Guess { correct, .. }) => {
                         if correct {
-                            self.statistic.correct.fetch_add(1, Ordering::Relaxed);
+                            let previous = self.statistic.correct.fetch_add(1, Ordering::Relaxed);
+                            self.statistic.events.notify(StatisticEvent::Correct(previous + 1)).await.expect("notify observers");
                         } else {
-                            self.statistic.incorrect.fetch_add(1, Ordering::Relaxed);
+                            let previous = self.statistic.incorrect.fetch_add(1, Ordering::Relaxed);
+                            self.statistic.events.notify(StatisticEvent::Incorrect(previous + 1)).await.expect("notify observers");
                         }
                     },
                     None => break,
@@ -75,13 +97,17 @@ mod test {
         let mut sender = Pharos::default();
         let receiver: StatisticReceiver = sender.observe(Channel::Unbounded.into()).await.expect("receive").into();
         let statistic = receiver.statistic.clone();
-        receiver.receive();
+        let statistic_events = statistic.observe(Channel::Unbounded.into()).await.expect("receive");
+        let handle = receiver.receive();
 
         yield_now().await;
 
         assert_eq!(statistic.correct(), 0, "no correct guess got counted");
         assert_eq!(statistic.incorrect(), 0, "no incorrect guess got counted");
         assert_eq!(statistic.total(), 0, "no guess got received");
+        handle.abort();
+        drop(statistic);
+        assert_eq!(statistic_events.count().await, 0, "no events left")
     }
 
     #[tokio::test]
@@ -89,14 +115,19 @@ mod test {
         let mut sender = Pharos::default();
         let receiver: StatisticReceiver = sender.observe(Channel::Unbounded.into()).await.expect("receive").into();
         let statistic = receiver.statistic.clone();
-        receiver.receive();
+        let mut statistic_events = statistic.observe(Channel::Unbounded.into()).await.expect("receive");
+        let handle = receiver.receive();
 
         sender.send(TrainerEvent::Guess { guess: "".to_owned(), correct: true }).await.expect("send correct guess");
         yield_now().await;
+        assert_eq!(statistic_events.next().await, Some(StatisticEvent::Correct(1)), "correct got updated to 1");
 
         assert_eq!(statistic.correct(), 1, "a correct guess got counted");
         assert_eq!(statistic.incorrect(), 0, "no incorrect guess got counted");
         assert_eq!(statistic.total(), 1, "a guess got received");
+        handle.abort();
+        drop(statistic);
+        assert_eq!(statistic_events.count().await, 0, "no events left")
     }
 
     #[tokio::test]
@@ -104,14 +135,19 @@ mod test {
         let mut sender = Pharos::default();
         let receiver: StatisticReceiver = sender.observe(Channel::Unbounded.into()).await.expect("receive").into();
         let statistic = receiver.statistic.clone();
-        receiver.receive();
+        let mut statistic_events = statistic.observe(Channel::Unbounded.into()).await.expect("receive");
+        let handle = receiver.receive();
 
         sender.send(TrainerEvent::Guess { guess: "".to_owned(), correct: false }).await.expect("send incorrect guess");
         yield_now().await;
+        assert_eq!(statistic_events.next().await, Some(StatisticEvent::Incorrect(1)), "correct got updated to 1");
 
         assert_eq!(statistic.correct(), 0, "no correct guess got counted");
         assert_eq!(statistic.incorrect(), 1, "an incorrect guess got counted");
         assert_eq!(statistic.total(), 1, "a guess got received");
+        handle.abort();
+        drop(statistic);
+        assert_eq!(statistic_events.count().await, 0, "no events left")
     }
 
     #[tokio::test]
@@ -119,7 +155,8 @@ mod test {
         let mut sender = Pharos::default();
         let receiver: StatisticReceiver = sender.observe(Channel::Unbounded.into()).await.expect("receive").into();
         let statistic = receiver.statistic.clone();
-        receiver.receive();
+        let statistic_events = statistic.observe(Channel::Unbounded.into()).await.expect("receive");
+        let handle = receiver.receive();
 
         sender.send(TrainerEvent::Select(None)).await.expect("send other event");
         yield_now().await;
@@ -127,5 +164,8 @@ mod test {
         assert_eq!(statistic.correct(), 0, "no correct guess got counted");
         assert_eq!(statistic.incorrect(), 0, "no incorrect guess got counted");
         assert_eq!(statistic.total(), 0, "no guess got received");
+        handle.abort();
+        drop(statistic);
+        assert_eq!(statistic_events.count().await, 0, "no events left")
     }
 }
